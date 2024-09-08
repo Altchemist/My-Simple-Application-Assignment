7fadd2031bc86b257a6dfeb4dd24bf87668ca26f
Hash <halchemist@tutamail.com>
2024-09-08 12:07:24 +0400

    feat: add a boilerplate server
    This server, is similiar to the simple TCP daytime server except it uses
    a different port number of 52000, as testbed. It also includes the
    header file

---
+++ server.h
@@ -0,0 +1,20 @@
+#ifndef __server_h
+#define __server_h
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#define LISTENQ 1024
+#define MAXLINE 4096
+#define SockAddr struct sockaddr
+
+/* Define bzero() as a macro if it's not in standard C library. */
+#ifndef	HAVE_BZERO
+#define	bzero(ptr,n)		memset(ptr, 0, n)
+/* $$.If bzero$$ */
+/* $$.If memset$$ */
+#endif
+
+#endif
