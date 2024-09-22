fdec0157ae2fb443f2eedfde9d9f54035ef9d07b
Hash <halchemist@tutamail.com>
2024-09-22 15:48:03 +0400

    feat: make server multithreaded

--- server_/serv.c
+++ server_/serv.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv)
     int listenfd, connfd;
     struct sockaddr_in servaddr;
     char buff[MAXLINE];
+    pid_t pid;
+
     time_t ticks;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -24,10 +26,15 @@ int main(int argc, char **argv)
     {
         connfd = accept(listenfd, (SockAddr*) NULL, NULL);
 
-        ticks = time(NULL);
-        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        write(connfd, buff, strlen(buff));
-
+        if ( (pid==fork()) == 0)
+        {
+            close(listenfd);
+            ticks = time(NULL);
+            snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+            write(connfd, buff, strlen(buff));
+            close(connfd);
+            exit(0);
+        }
         close(connfd);
     }
 }
