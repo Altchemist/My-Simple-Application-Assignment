01a864b03ca3cf873ba9a38956735c625cc847c1
Hash <halchemist@tutamail.com>
2024-09-27 22:03:26 +0400

    feat: implement command validation
    The command validation uses regex to match the string pattern for SUM
    and FILES

--- client/client.h
+++ client/client.h
@@ -11,9 +11,15 @@
 #include <string.h>
 #include <termios.h>
 #include <termio.h>
+#include <regex.h>
 
 #define	bzero(ptr,n)		memset(ptr, 0, n)
 #define SockAddr struct sockaddr
+#define IS_TIME strcmpr(sendBuffer, "TIME")==0
+#define IS_USERS strcmpr(sendBuffer, "USERS")==0
+#define IS_EXIT strcmpr(sendBuffer, "EXIT")==0
+#define IS_FILES match_file==0
+#define IS_SUM match_sum==0
 
 #define MAXLINE 4096
 
